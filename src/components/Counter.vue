46577f96cae1f362e66abe8bda173cbade239dbb
Fufu <fuyuwu041000@gmail.com>
2023-04-19 22:51:46 +0800

    fix: fix runtime complier

---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p @click="counter += 1">你已經按下了{{ counter }}次</p>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'CounterComponent',
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+}
+</script>
--- src/components/Counting.vue
+++ src/components/Counting.vue
@@ -2,25 +2,22 @@
   <div id="CountingComponent">
     <p @click="counter += 1">你已經按下了{{ counter }}次</p>
     <p @click="counter += 1">你已經按下了{{ counter }}次</p>
-    <counter-component></counter-component>
-    <counter-component></counter-component>
-    <counter-component></counter-component>
-    <!-- TODO:為何沒出現啊 -->
+    <!-- 元件分開寫就對了 -->
+    <CounterComponent/>
+    <CounterComponent/>
+    <CounterComponent/>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-Vue.component('counter-component', {
-  data() {
-    return {
-      counter: 0
-    }
-  },
-  template: `<div><p @click="counter+=1">你已經按下了{{ counter }}次</p></div>`
-})
+
+import CounterComponent from './Counter.vue'
+
 export default {
   name: 'CountingComponent',
+  components: {
+    CounterComponent
+  },
   data() {
     return {
       counter: 0
